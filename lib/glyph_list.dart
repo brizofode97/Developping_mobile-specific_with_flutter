9adf1677788a2f3f6e9b9aeaaf83ef9273483db8
brizofode97 <brizofode97@gmail.com>
2023-01-12 17:57:16 +0100

    The audios glyphs

--- lib/glyph_list.dart
+++ lib/glyph_list.dart
@@ -2,19 +2,30 @@ import 'package:flutter/material.dart';
 
 import 'dart:convert';
 
+import 'package:audioplayers/audioplayers.dart';
+
 class GlyphListWidget extends StatefulWidget {
   const GlyphListWidget({super.key});
-
 
   @override
   State<GlyphListWidget> createState() {
     return _GlyphList();
   }
-
 }
 
 class _GlyphList extends State<GlyphListWidget> {
   late Future<List<dynamic>> glyphs;
+
+  //late AudioCache _audioCache;
+  final List<String> route = [
+    'audio/glyphs/a.mp3',
+    'audio/glyphs/e.mp3',
+    'audio/glyphs/i.mp3',
+    'audio/glyphs/o.mp3',
+    'audio/glyphs/u.mp3',
+  ];
+  final player = AudioPlayer();
+  int i = 0;
 
   Future<List<dynamic>> fetchList() async {
     //await Future.delayed(Duration(seconds: 5));
@@ -27,6 +38,8 @@ class _GlyphList extends State<GlyphListWidget> {
   void initState() {
     super.initState();
     glyphs = fetchList();
+
+    //_audioCache =  AudioCache(prefix: "assets/audio/glyphs/", fixedPlayer: AudioPlayer());
   }
 
   @override
@@ -40,35 +53,51 @@ class _GlyphList extends State<GlyphListWidget> {
               children: <Widget>[
                 // Should check that the type of glyph
                 // is effectively a Map and contains the keys needed
-                if (glyphInfo != null) for (var glyph in glyphInfo) ListTile(
-                  leading: Text(glyph["glyph"],
-                      style: const TextStyle(fontSize: 39.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blueGrey)),
-                  title: Text(glyph["info"],
-                      style: const TextStyle(fontSize: 11.0,
-                          fontWeight: FontWeight.normal,
-                          color: Colors.blueAccent))
-                ),
+                if (glyphInfo != null)
+                  for (var glyph in glyphInfo)
+                    ListTile(
+                        leading: Text(glyph["glyph"],
+                            style: const TextStyle(
+                                fontSize: 39.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blueGrey)),
+                        title: Text(glyph["info"],
+                            style: const TextStyle(
+                                fontSize: 11.0,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.blueAccent)),
+                        trailing: IconButton(
+                          color: Colors.orange,
+                          icon: const Icon(Icons.play_circle_outline),
+                          onPressed: () => _playShortSound("audio/glyphs/${glyph["audio"]}") ,
+                        )),
               ],
             );
           } else if (snapshot.hasError) {
             return const Center(
               child: Text("Error! Could not load glyph data!",
-                  style: TextStyle(fontSize: 19.0,
+                  style: TextStyle(
+                      fontSize: 19.0,
                       fontWeight: FontWeight.bold,
                       color: Colors.red)),
             );
           } else {
             return const Center(
               child: Text("Loading glyph data...",
-                  style: TextStyle(fontSize: 19.0,
+                  style: TextStyle(
+                      fontSize: 19.0,
                       fontWeight: FontWeight.bold,
                       color: Colors.grey)),
             );
           }
-        }
-    );
+        });
   }
 
+  void _playShortSound(String asset) {
+    player.stop();
+    // ignore: unrelated_type_equality_checks
+    if(player.state != 0){
+      player.play(AssetSource(asset));
+    }
+  }
 }
